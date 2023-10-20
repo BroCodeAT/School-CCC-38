72a0d14bb73883e2b341f1887b754467b2c7b8b2
dpfurner <dpfurner@tsn.at>
2023-10-20 10:40:44 +0200

    Level 3

--- level3/level3.py
+++ level3/level3.py
@@ -1,19 +1,72 @@
-def load_file(filename: str = "level3/level3_1.in") -> list:
+from dataclasses import dataclass
+
+
+def load_file(filename: str = "level2/level2_1.in") -> list:
     with open(filename, "r") as file:
-        return [line.strip() for line in file.readlines()]
+        return [line.strip() for line in file.readlines()][1:]
 
 
-def write_to_file(data: list, filename: str = "level3/level3_1.out") -> None:
+def write_to_file(pieces, counts, filename: str = "level2/level2_1.out") -> None:
     with open(filename, "w") as file:
-        for line in data:
-            file.write(line + "\n")
+        for piece, count in zip(pieces, counts):
+            file.write(str(count) + " " + str(piece) + "\n")
 
 
-def main(data: list) -> list:
-    return data
+@dataclass
+class Piece:
+    top: bool
+    right: bool
+    bottom: bool
+    left: bool
+
+    rotated = 0
+
+    def __str__(self):
+        return f"{'K' if self.top else 'H'},{'K' if self.right else 'H'},{'K' if self.bottom else 'H'},{'K' if self.left else 'H'}"
+
+    def __eq__(self, other: "Piece"):
+        if self.top == other.top and self.right == other.right and self.bottom == other.bottom and self.left == other.left:
+            return True
+        return False
+
+    def __init__(self, data: str):
+        data = data.split(",")
+        self.top = True if data[0] == "K" else False
+        self.right = True if data[1] == "K" else False
+        self.bottom = True if data[2] == "K" else False
+        self.left = True if data[3] == "K" else False
+
+    def rotate(self):
+        self.top, self.right, self.bottom, self.left = self.left, self.top, self.right, self.bottom
+        self.rotated += 1
+        if self.rotated == 4:
+            self.rotated = 0
+
+
+def main(data: list):
+    data = [Piece(piece) for piece in data]
+    pieces = []
+    counts = []
+    for piece in data:
+        in_flag = False
+        for i in range(4):
+            if piece in pieces:
+                in_flag = True
+                counts[pieces.index(piece)] += 1
+                break
+            else:
+                piece.rotate()
+        if not in_flag:
+            pieces.append(piece)
+            counts.append(1)
+    # Count if a piece is equal to another piece in any rotation and count them
+    return counts, pieces
 
 
 if __name__ == '__main__':
+    count, pieces = main(load_file(f"level2/level2_example.in"))
+    write_to_file(pieces, count, f"level2/level2_example.out")
+
     for i in range(1, 6):
-        comp = main(load_file(f"level3/level3_{i}.in"))
-        write_to_file(comp, f"level3/level3_{i}.out")
+        count, pieces = main(load_file(f"level2/level2_{i}.in"))
+        write_to_file(pieces, count, f"level2/level2_{i}.out")
