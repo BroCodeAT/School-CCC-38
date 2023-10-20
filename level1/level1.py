556bc977dd4c24faa16a7954fd8de1bbe91ae3cc
dpfurner <dpfurner@tsn.at>
2023-10-20 10:09:45 +0200

    School CCC-38

--- level1/level1.py
+++ level1/level1.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
+
 def load_file(filename: str = "level1/level1_1.in") -> list:
     with open(filename, "r") as file:
-        return [line.strip() for line in file.readlines()]
+        return [line.strip() for line in file.readlines()][1:]
 
 
 def write_to_file(data: list, filename: str = "level1/level1_1.out") -> None:
@@ -14,6 +16,6 @@ def main(data: list) -> list:
 
 
 if __name__ == '__main__':
-    for i in range(1, 6):
+    for i in range(1, 2):
         comp = main(load_file(f"level1/level1_{i}.in"))
         write_to_file(comp, f"level1/level1_{i}.out")
