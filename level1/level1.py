69d738a8db99975222bdaba26979a20fccc54f9d
dpfurner <dpfurner@tsn.at>
2023-10-20 10:15:55 +0200

    School CCC-38

--- level1/level1.py
+++ level1/level1.py
@@ -1,21 +1,26 @@
-from dataclasses import dataclass
-
 def load_file(filename: str = "level1/level1_1.in") -> list:
     with open(filename, "r") as file:
         return [line.strip() for line in file.readlines()][1:]
 
 
-def write_to_file(data: list, filename: str = "level1/level1_1.out") -> None:
+def write_to_file(pieces, counts, filename: str = "level1/level1_1.out") -> None:
     with open(filename, "w") as file:
-        for line in data:
-            file.write(line + "\n")
+        for piece, count in zip(pieces, counts):
+            file.write(str(count) + " " + piece + "\n")
 
 
-def main(data: list) -> list:
-    return data
+def main(data: list):
+    pieces:list[str] = []
+    counts:list[int] = []
+    for line in data:
+        if line not in pieces:
+            pieces.append(line)
+    for piece in pieces:
+        counts.append(data.count(piece))
+    return pieces, counts
 
 
 if __name__ == '__main__':
     for i in range(1, 2):
-        comp = main(load_file(f"level1/level1_{i}.in"))
-        write_to_file(comp, f"level1/level1_{i}.out")
+        pieces, counts = main(load_file(f"level1/level1_example.in"))
+        write_to_file(pieces, counts, f"level1/level1_example.out")
--- level2/level2.py
+++ level2/level2.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+
+
 def load_file(filename: str = "level2/level2_1.in") -> list:
     with open(filename, "r") as file:
         return [line.strip() for line in file.readlines()]
@@ -9,7 +12,28 @@ def write_to_file(data: list, filename: str = "level2/level2_1.out") -> None:
             file.write(line + "\n")
 
 
+@dataclass
+class Piece:
+    top: bool
+    right: bool
+    bottom: bool
+    left: bool
+
+    def __eq__(self, other: "Piece"):
+        if self.top == other.top and self.right == other.right and self.bottom == other.bottom and self.left == other.left:
+            return True
+        return False
+
+    def __init__(self, data: str):
+        data = data.split(",")
+        self.top = True if data[0] == "K" else False
+        self.right = True if data[1] == "K" else False
+        self.bottom = True if data[2] == "K" else False
+        self.left = True if data[3] == "K" else False
+
+
 def main(data: list) -> list:
+    pieces = [Piece(piece) for piece in data]
     return data
 
 
